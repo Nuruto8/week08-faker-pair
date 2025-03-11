fd227b44028c54d718e63a4483d479265bc82ff9
Bry <etoquillab@gmail.com>
2025-03-11 12:04:15 +0800

    removed the sql haha

--- user_profiles.php
+++ user_profiles.php
@@ -2,32 +2,8 @@
 require 'vendor/autoload.php';
 $faker = Faker\Factory::create('en_PH');
 
-$host = "localhost";
-$username = "root";
-$password = "202280287PSU";
-$database = "faker";
 
-$conn = new mysqli($host, $username, $password, $database);
-if ($conn->connect_error){
-    die("Connection failed" . $conn->connect_error);
 
-}
-
-for ($kb = 0; $kb < 5; $kb++){
-    $full_name = $faker->name();
-    $email = $faker->email();
-    $phone_number = "+63 " . $faker->numerify("9## ### ####");
-    $address = $faker->address();
-    $birthdate = $faker->date('Y-m-d');
-    $job_title = $faker->jobTitle();
-
-    $stmt = $conn->prepare("INSERT INTO users (full_name, email, phone_number, address, birthdate, job_title) VALUES (?, ?, ?, ?, ?, ?)");
-    $stmt->bind_param("ssssss", $full_name, $email, $phone_number, $address, $birthdate, $job_title);
-    $stmt->execute();
-    $stmt->close();
-}
-
-$result = $conn->query("SELECT * FROM users ORDER BY id DESC LIMIT 5");
 
 echo "<!DOCTYPE html>
 <html lang='en'>
@@ -50,21 +26,21 @@ echo "<!DOCTYPE html>
             <th>Job Title</th>
         </tr>";
 
-        while ($row = $result->fetch_assoc()) {
-            echo "<tr>
-                <td>{$row['id']}</td>
-                <td>{$row['full_name']}</td>
-                <td>{$row['email']}</td>
-                <td>{$row['phone_number']}</td>
-                <td>{$row['address']}</td>
-                <td>{$row['birthdate']}</td>
-                <td>{$row['job_title']}</td>
-            </tr>";
-        }
+        for ($kb = 1; $kb < 6; $kb++){
+            echo"<tr>
+            <td>{$kb}</td>
+            <td>{$faker->name()}</td>
+            <td>{$faker->email()}</td>
+            <td>+63 {$faker->numerify('9## ### ####')}</td>
+            <td>{$faker->address()}</td>
+            <td>{$faker->date('Y-m-d')}</td>
+            <td>{$faker->jobTitle()}</td>
+                </tr>";
+             }
 
 echo "</table></body></html>";
 
-$conn->close();
+
 
 
 ?>
