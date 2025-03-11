ac5171456b49e75b4ef6193df8893e6f7cbd6529
Bry <etoquillab@gmail.com>
2025-03-11 11:23:34 +0800

    create a ignore file

--- user_profiles.php
+++ user_profiles.php
@@ -5,12 +5,14 @@ $faker = Faker\Factory::create('en_PH');
 $host = "localhost";
 $username = "root";
 $password = "202280287PSU";
-$database = "faker"
+$database = "faker";
 
 $conn = new mysqli($host, $username, $password, $database);
 if ($conn->connect_error){
     die("Connection failed" . $conn->connect_error);
 }
+
+
 
 
 
